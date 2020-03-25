6d8cd9aee55a63c81110758eec16c388e05646c1
momi <mohamad97mj@gmail.com>
2020-03-25 12:15:00 +0430

    ports updated

--- client/client-app/src/my-axios.js
+++ client/client-app/src/my-axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'http://87.107.134.134:5000/api'
 });
 
 export default instance;

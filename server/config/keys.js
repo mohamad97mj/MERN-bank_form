94abc17e629c034931fb9ca0b23ef7d032171302
momi <mohamad97mj@gmail.com>
2020-03-29 12:55:59 +0430

    password to db corrected

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://mohamad97mj:604998@localhost:27017/users',
+  mongoURI: 'mongodb://mohamad97mj:mJ604998@localhost:27017/users',
   secretOrKey: "secret"
 };

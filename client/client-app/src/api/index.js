94925f65d1f8b8b3e0e10aa44eff86e073e06881
mohamad <mohamad97mj@gmail.com>
2020-05-28 11:12:35 +0430

    bank-exchange last version

---
+++ client/client-app/src/api/index.js
@@ -0,0 +1,28 @@
+export {
+    auth,
+    register,
+    logout,
+    setAuthRedirectPath,
+    authCheckState
+} from '../store/actions/auth';
+
+export {
+    postForm,
+    onChangeHandler,
+    addContentHandler,
+    removeContentHandler,
+    contentLevel1onChangeHandler,
+    addContentLevel1Handler,
+    removeContentLevel1Handler,
+    contentLevel2onChangeHandler,
+    addContentLevel2Handler,
+    removeContentLevel2Handler,
+    contentLevel3onChangeHandler,
+    disableRemittanceBanksNameHandler,
+    disableRemittanceBanksHandler
+} from '../store/actions/main';
+
+export {
+    postProfile, profileOnChangeHandler
+} from '../store/actions/profile';
+
